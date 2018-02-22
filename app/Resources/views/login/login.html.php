d59521fb8d84d3261dbf1d514306a48ef433ab66
lotfi1002 <lotfi1002@gmail.com>
2018-02-22 14:42:34 +0000

    1

--- app/Resources/views/login/login.html.php
+++ app/Resources/views/login/login.html.php
@@ -5,9 +5,9 @@
 <body>
 <form action="" method="post">
 <label> Login : </label>
-<input name="username " id="username " type="text" />
+<input name="username" id="username " type="text" /></br>
 <label> Password : </label>
-<input name="plainPassword" id="plainPassword" type="password"  />
+<input name="plainPassword" id="plainPassword" type="password"  /></br>
 
 <input name="ok" id="ok" type="submit" value="Login!"/>
 </form>
