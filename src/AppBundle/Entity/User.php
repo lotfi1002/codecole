d6baf21b0332084176bb6be12ca27ac29f050d65
lotfi1002 <lotfi1002@gmail.com>
2018-02-23 14:05:28 +0000

    1

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -83,7 +83,7 @@ class User implements UserInterface
 
     public function getRoles()
     {
-        return array('ROLE_USER');
+        return array('ROLE_PROF','ROLE_ADMIN');
     }
 
     public function eraseCredentials()
