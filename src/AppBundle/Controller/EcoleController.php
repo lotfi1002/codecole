9df2d6dcfed9bef77bff09c9fe79727d8bad7fa6
lotfi1002 <lotfi1002@gmail.com>
2018-04-19 22:33:38 +0000

    toto

--- src/AppBundle/Controller/EcoleController.php
+++ src/AppBundle/Controller/EcoleController.php
@@ -9,9 +9,29 @@ use AppBundle\Entity\Ecole;
 
 class EcoleController extends Controller
 {
-
+	
 	/**
      * @Route("/myspace/ecole")
+     */
+    public function ecoleAction(Request $request)
+    {
+		 $user = null ;
+		if( $this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY') ){
+			$user = $this->container->get('security.token_storage')->getToken()->getUser();
+			$em = $this->getDoctrine()->getManager();
+			
+	
+		
+		}
+			
+			 return $this->render('myspace/addcole.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),'user'=> $user,
+        ));
+       
+    }
+
+	/**
+     * @Route("/myspace/ecoles")
      */
     public function listEcoleAction(Request $request)
     {
@@ -36,7 +56,32 @@ class EcoleController extends Controller
         ));
        
     }
-}
+
+	
+	/**
+     * @Route("/myspace/saveecole")
+     */
+    public function saveAction(Request $request)
+    {
+		 $user = null ;
+		if( $this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY') ){
+			$user = $this->container->get('security.token_storage')->getToken()->getUser();
+			$em = $this->getDoctrine()->getManager();
+			
+	
+		
+		}
+			
+			 return $this->render('myspace/addcole.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),'user'=> $user,
+        ));
+       
+    }
+
+	
+	
+	
+	}
 
 
 ?>
