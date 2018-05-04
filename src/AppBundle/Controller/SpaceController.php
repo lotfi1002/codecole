6b8811cc7fd4ae03ccbf6bd6ae7c6237d10fef26
yasmiineks <37066490+yasmiineks@users.noreply.github.com>
2018-05-04 17:53:58 +0100

    ecole-test

--- src/AppBundle/Controller/SpaceController.php
+++ src/AppBundle/Controller/SpaceController.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Etudiant;
 
 
 class SpaceController extends Controller
@@ -103,9 +105,61 @@ class SpaceController extends Controller
 		if( $this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY') ){
 			$user = $this->container->get('security.token_storage')->getToken()->getUser();
 			$em = $this->getDoctrine()->getManager();
-			
-		
-		
+		}
+
+		if ($request->isMethod('post')){
+			if ($request->get('cne') == '' || $request->get('cmasar') == '' || $request->get('nom') =='' || $request->get('prenom') == '' || $request->get('nomfr') == '' 
+				|| $request->get('prenomfr') == '' || $request->get('sex') == '' || $request->get('cin') == '' || $request->get('code') == '' || $request->get('adresse') == '')
+			{
+							 return $this->render('myspace/ecolev3.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'user'=> $user,
+            'ErrTitle' => 'Erreur !!',
+            'ErrMsg' => 'Les champs suivi par * sont obligatoire', 
+        ));
+			}
+			else {
+				$em = $this->getDoctrine()->getManager();
+				$rep = $this->getDoctrine()->getRepository(Etudiant::class);
+				$etd = new Etudiant();
+				$etd->setCne($request->get('cne'));
+				$etd->setIdMassar($request->get('cmasar'));
+				$etd->setNom($request->get('nom'));
+				$etd->setPrenom($request->get('prenom'));
+				$etd->setNomfr($request->get('nomfr'));
+				$etd->setPrenomfr($request->get('prenomfr'));
+				$etd->setDate(new \Datetime($request->get('date') ));
+				$etd->setSex($request->get('sex'));
+				$request->get('lieu1') == 'Autre' ? $etd->setAutre1($request->get('autre1')) : $etd->setLieu1($request->get('lieu1'));
+				$request->get('lieu2') == 'Autre' ? $etd->setAutre2($request->get('autre2')) : $etd->setLieu2($request->get('lieu2'));
+				$request->get('nat') == 'Autre' ? $etd->setNat($request->get('nat2')) : $etd->setNat($request->get('nat'));
+				$etd->setCin($request->get('cin'));
+				$etd->setMatricule($request->get('matricule'));
+				$etd->setStatut($request->get('statut'));
+				$etd->setNiveau($request->get('niveau'));
+				$etd->setNiveau2($request->get('niveau2'));
+				$etd->setClasse($classe=$request->get('classe'));
+				$etd->setRed($request->get('red'));
+				$etd->setTransport($request->get('transport'));
+				$etd->setDate2(new \Datetime($request->get('date2') ));
+				$etd->setNiveau3($request->get('niveau3'));
+				$etd->setEtablissement($request->get('etablissement'));
+				$etd->setRad3($request->get('rad3'));
+				$etd->setCode($request->get('code'));
+				$etd->setVille($request->get('ville'));
+				$etd->setAdresse($request->get('adresse'));
+				$etd->setTel($request->get('telephone'));
+				$etd->setPortable($request->get('portable'));
+				$etd->setEmail($request->get('email'));
+				$etd->setDate3(new \Datetime($request->get('date3') ));
+				$etd->setMotif($request->get('motif'));
+				$etd->setEtablissement2($request->get('etablissement2'));
+				$etd->setRad10($request->get('rad10'));
+				$etd->setCode2($request->get('code2'));
+				$etd->setVille2($request->get('ville2'));
+				$em->persist($etd);
+				$em->flush();
+			}
 		}
 			
 			 return $this->render('myspace/ecolev3.html.twig', array(
@@ -370,6 +424,26 @@ class SpaceController extends Controller
 		}
 			
 			 return $this->render('myspace/editionregistre.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),'user'=> $user,
+        ));
+       
+    }
+             /**
+     * @Route("/myspace/recoleIns" , name="ecoleIns")
+     */
+    public function  ecoleInsAction(Request $request)
+    {
+		 $user = null ;
+		
+		if( $this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY') ){
+			$user = $this->container->get('security.token_storage')->getToken()->getUser();
+			$em = $this->getDoctrine()->getManager();
+			
+		
+		
+		}
+			
+			 return $this->render('myspace/ecoleIns.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),'user'=> $user,
         ));
        
